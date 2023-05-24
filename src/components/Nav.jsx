508cd96113a833e8c4bf9623e9c167c4938fa30e
Phoenix Chang <ph90630@gmail.com>
2023-05-24 19:54:42 +0800

    修改部分code使NavBar可以顯示當前頁面

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-restricted-globals */
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Nav = () => {
+  const [page, setPage] = useState('home');
+  const navigate = useNavigate();
+  const toggle = (idRemove, idAdd, idPage = '') => {
+    document.querySelector(`#${idRemove}`).classList.remove('active');
+    document.querySelector(`#${idAdd}`).classList.add('active');
+    if (idPage !== '') {
+      setPage(idPage);
+    }
+  };
+  const handleNav = (e) => {
+    let target = e.target.closest('.link');
+
+    setPage(target.id);
+    if (page) {
+      document.querySelector(`#${page}`).classList.remove('active');
+    }
+    document.querySelector(`#${target.id}`).classList.add('active');
+    if (target.id === 'home') {
+      navigate('/photoweb');
+    } else if (target.id === 'about') {
+      navigate('/about');
+    }
+  };
+  useEffect(() => {
+    if (
+      location.pathname === '/' ||
+      location.pathname === '' ||
+      location.pathname === '/photoweb'
+    ) {
+      toggle('about', 'home', 'home');
+    } else {
+      toggle('home', 'about', 'about');
+    }
+  }, []);
   return (
     <nav>
       <ul>
-        <li>
+        <li className='link active' id='home' onClick={handleNav}>
           <Link to='/'>Home</Link>
         </li>
-        <li>
+        <li className='link' id='about' onClick={handleNav}>
           <Link to='/about'>About</Link>
         </li>
       </ul>
